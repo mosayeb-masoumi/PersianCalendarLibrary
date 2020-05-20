a38d8f4431dd8cd09b36bc66a86ecb8532bcd8ba
mosayeb_masoumi <mosayeb.masoumi.co@gmail.com>
2020-05-20 19:47:02 +0430

    v1.1.4

--- app/src/androidTest/java/com/example/persiancalendar/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/persiancalendar/ExampleInstrumentedTest.java
@@ -1,10 +1,8 @@
 package com.example.persiancalendar;
 
 import android.content.Context;
-
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
